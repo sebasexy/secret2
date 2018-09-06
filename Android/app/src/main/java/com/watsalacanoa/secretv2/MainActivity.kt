c89c6c9dad95c39d3d54d58529413356ecb0752f
sebasexy <spider_cbaz_10@hotmail.com>
2018-09-06 13:04:16 -0500

    Working on AlertDialog Builder

--- Android/app/src/main/java/com/watsalacanoa/secretv2/MainActivity.kt
+++ Android/app/src/main/java/com/watsalacanoa/secretv2/MainActivity.kt
@@ -1,28 +1,83 @@
 package com.watsalacanoa.secretv2
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
+
+    private class Content(context: Context): BaseAdapter(){
+
+        private val mContext: Context
+
+        init {
+            mContext = context
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val textView = TextView(mContext)
+            textView.text = "Hola"
+            return textView
+        }
+
+        override fun getItem(p0: Int): Any {
+            return "Test String"
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return 0
+        }
+
+        override fun getCount(): Int {
+            return 5
+        }
+
+
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        var elements_to_display: Int = 0
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+        supportActionBar?.setTitle("Tablon")
 
-        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val listView = findViewById<ListView>(R.id.main_listView)
+        listView.adapter = Content(this)
+
+
+        var btnAddElement = findViewById<View>(R.id.btnAddNewElement)
+        btnAddElement.setOnClickListener{
+            val mBuilder = AlertDialog.Builder(this@MainActivity)
+
+            var mView = LayoutInflater.from(this).inflate(R.layout.dialog_new_comment, null)
+            var mComment = mView.findViewById<EditText>(R.id.idTextNewComment)
+            var btnShare = mView.findViewById<Button>(R.id.btnShare)
+            var btnCancel = mView.findViewById<Button>(R.id.btnCancel)
+
+            btnShare.setOnClickListener{
+                if(!mComment.text.isEmpty()){
+                    Toast.makeText(this, mComment.text, Toast.LENGTH_SHORT).show()
+                }
+            }
+            mBuilder.setView(mView)
+
         }
+
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -40,4 +95,6 @@ class MainActivity : AppCompatActivity() {
             true
         } else super.onOptionsItemSelected(item)
     }
+
+
 }
